6a0235e3b864f3bbeec4b596100fcb8f5c7add45
subhroy <subhasis.it@gmail.com>
2022-08-24 11:14:31 +0530

    Made the Swagger values Dynamic in OpenAPIConfig

--- src/main/java/com/training/lab/config/OpenAPIConfig.java
+++ src/main/java/com/training/lab/config/OpenAPIConfig.java
@@ -3,11 +3,19 @@ package com.training.lab.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenAPIConfig {
+
+  @Value("${app.swagger.title}")
+  private String title;
+  @Value("${app.swagger.version}")
+  private String version;
+  @Value("${app.swagger.description}")
+  private String description;
 
   @Bean
   public OpenAPI customOpenAPI() {
@@ -15,8 +23,9 @@ public class OpenAPIConfig {
         .components(new Components())
         .info(
             new Info()
-                .title("Home Spring Boot APIs")
-                .version("1.0.0")
-                .description("API documentation for Home Project..!!"));
+                .title(this.title)
+                .version(this.version)
+                .description(this.description));
   }
+
 }
